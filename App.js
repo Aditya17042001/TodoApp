1c43bb5084e558e3cdc5d615245354b7d961121f
Aditya17042001 <126761920+Aditya17042001@users.noreply.github.com>
2023-08-16 17:09:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+
+export default function App() {
+  const [todos, setTodos]=useState([
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'},
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'},
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'},
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'},
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'}
+
+  ]);
+  const pressHandler=(key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo=>todo.key!=key)
+    });
+  }
+  return (
+    <View style={styles.container}>
+    <Header></Header>
+     <View style={styles.content}>
+      <View style={styles.list}>
+         <FlatList 
+         data={todos}
+         renderItem={({item})=>(
+        <TodoItem item={item} pressHandler={pressHandler}/>
+         )} />
+        
+      </View>
+     </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    
+  },
+  content:{
+    padding:20,
+    borderColor:'red',
+    margin:5,
+   
+  },
+
+});
